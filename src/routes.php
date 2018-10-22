d3923ae4b83b31b62fdc9470f647b193730140b8
Sam Warley <samwarley@Sams-MacBook-Pro.local>
2018-10-22 18:20:18 -0500

    Added product filtered API Route

--- src/routes.php
+++ src/routes.php
@@ -19,6 +19,37 @@ $app->group('/api', function () use ($app) {
 		}
 	);
 
+	$app->get('/productsFiltered', 
+		function ($request, $response, $args) {
+			$db = $this->dbConn;
+			$sql= $db->prepare(
+				"SELECT * from Products WHERE 1=1"
+			);
+
+			if ($input['color']) {
+			    $sql .= " (color = :color";
+			}
+			if ($input['style']) {
+			    $sql .= " AND style = :style";
+			}
+			if ($input['materials']) {
+			    $sql .= " AND materials = :materials";
+			}
+			if ($input['brand']) {
+			    $sql .= " AND brand = :brand";
+			}
+			$sql .= ")";
+
+			$sql->bindParam("color", $input['color']);
+			$sql->bindParam("style", $input['style']);
+			$sql->bindParam("materials", $input['materials']);
+			$sql->bindParam("brand", $input['brand']);
+			$sql->execute();
+			$products = $sql->fetchAll(PDO::FETCH_ASSOC);
+			return $this->response->withJson($products);
+		}
+	);
+
 	$app->get('/products', 
 		function ($request, $response, $args) {
 			$db = $this->dbConn;
