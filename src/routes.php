8cfe6c37396cc1dad98e114aadde2eb8a739e449
Sam Warley <samwarley@Sams-MacBook-Pro.local>
2018-10-31 17:25:38 -0500

    register update v2

--- src/routes.php
+++ src/routes.php
@@ -29,7 +29,7 @@ $app->group('/api', function () use ($app) {
 			$sth->bindParam("first_name", $input['first_name']);
 			$sth->bindParam("last_name", $input['last_name']);
 			$sth->execute();
-			$sql2 = "SELECT id, first_name, email, last_name FROM 'Users' WHERE 'id' = LAST_INSERT_ID()";
+			$sql2 = "SELECT id, first_name, email, last_name FROM Users WHERE id = LAST_INSERT_ID()";
 			$sth2 = $this->dbConn->prepare($sql2);
 			$sth2->execute();
 			$registerOut = $sth2->fetchObject();
