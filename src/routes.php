489b656b2387998b1a3d4d011ef91c5e00b62a72
Sam Warley <samwarley@Sams-MacBook-Pro.local>
2018-11-02 16:36:30 -0500

    Register returns -1 value if email is already found

--- src/routes.php
+++ src/routes.php
@@ -22,18 +22,27 @@ $app->group('/api', function () use ($app) {
 	$app->post('/register',
 		function ($request, $response) {
 			$input = $request->getParsedBody();
-			$sql = "INSERT INTO Users (password, email, first_name, last_name) VALUES ( SHA1(:password), :email, :first_name, :last_name)";
-			$sth = $this->dbConn->prepare($sql);
-			$sth->bindParam("email", $input['email']);
-			$sth->bindParam("password", $input['password']);
-			$sth->bindParam("first_name", $input['first_name']);
-			$sth->bindParam("last_name", $input['last_name']);
-			$sth->execute();
-			$sql2 = "SELECT id, first_name, email, last_name FROM Users WHERE id = LAST_INSERT_ID()";
-			$sth2 = $this->dbConn->prepare($sql2);
-			$sth2->execute();
-			$registerOut = $sth2->fetchObject();
-			return $this->response->withJson($registerOut);
+			$sqlCheck = "SELECT EXISTS(SELECT email FROM Users WHERE email = :email)";
+			$sthCheck = $this->dbConn->prepare($sqlCheck);
+			$sthCheck->execute();
+			$sqlCheckResult = $sthCheck->fetchObject();
+			if($sqlCheckResult == 1) {
+				$errorCode = -1;
+				return $this->response->withJson($errorCode);
+			} else {
+				$sql = "INSERT INTO Users (password, email, first_name, last_name) VALUES (SHA1(:password), :email, :first_name, :last_name)";
+				$sth = $this->dbConn->prepare($sql);
+				$sth->bindParam("email", $input['email']);
+				$sth->bindParam("password", $input['password']);
+				$sth->bindParam("first_name", $input['first_name']);
+				$sth->bindParam("last_name", $input['last_name']);
+				$sth->execute();
+				$sql2 = "SELECT id, first_name, email, last_name FROM Users WHERE id = LAST_INSERT_ID()";
+				$sth2 = $this->dbConn->prepare($sql2);
+				$sth2->execute();
+				$registerOut = $sth2->fetchObject();
+				return $this->response->withJson($registerOut);
+			}
 		}
 	);
 
@@ -70,6 +79,45 @@ $app->group('/api', function () use ($app) {
 			$sth->execute();
 			$products = $sth->fetchAll(PDO::FETCH_ASSOC);
 			return $this->response->withJson($products);
+		}
+	);
+
+	$app->post('/follow',
+		function ($request, $response) {
+			$input = $request->getParsedBody();
+			$sql = "INSERT INTO Follows (user_id, brand_id) VALUES (:user_id, :brand_id)";
+			$sth = $this->dbConn->prepare($sql);
+			$sth->bindParam("user_id", $input['user_id']);
+			$sth->bindParam("brand_id", $input['brand_id']);
+			$sth->execute();
+			$followPost = $sth->fetchObject();
+			return $this->response->withJson($followPost);
+		}
+	);
+	$app->delete('/unfollow',
+		function ($request, $response) {
+			$input = $request->getParsedBody();
+			$sql = "DELETE FROM Follows WHERE user_id = :user_id AND brand_id = :brand_id";
+			$sth = $this->dbConn->prepare($sql);
+			$sth->bindParam("user_id", $input['user_id']);
+			$sth->bindParam("brand_id", $input['brand_id']);
+			$sth->execute();
+			$unfollowDelete = $sth->fetchObject();
+			return $this->response->withJson($unfollowDelete);
+		}
+	);
+	$app->post('/viewed',
+		function ($request, $response) {
+			$input = $request->getParsedBody();
+			$sql = "INSERT INTO User_Viewed (user_id, prod_id, date, id) VALUES (:user_id, :prod_id, :date, :id)";
+			$sth = $this->dbConn->prepare($sql);
+			$sth->bindParam("user_id", $input['user_id']);
+			$sth->bindParam("prod_id", $input['prod_id']);
+			$sth->bindParam("date", $input['date']);
+			$sth->bindParam("id", $input['id']);
+			$sth->execute();
+			$viewedProduct = $sth->fetchObject();
+			return $this->response->withJson($viewedProduct);
 		}
 	);
 
